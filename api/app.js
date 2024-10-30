d0b24bc3389fd54fe8ad31d03ae971b64872d662
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2024-10-30 11:40:33 +0100

    routing

--- api/app.js
+++ api/app.js
@@ -5,8 +5,12 @@ import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
-// import chatRoute from "./routes/chat.route.js";
-// import messageRoute from "./routes/message.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+import path from "path";
+
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -18,8 +22,14 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
-// app.use("/api/chats", chatRoute);
-// app.use("/api/messages", messageRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist' , 'index.html'));
+})
 
 app.listen(8800, () => {
   console.log("Server is running!!!");
